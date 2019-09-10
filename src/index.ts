dd5620c41654d48eb7b1b54a71bf7b1e1529fefb
David Pohan <0ronon0@gmail.com>
2019-09-10 11:30:13 +0200

    Add access logging

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { exec } from 'child_process';
 import { htmlToPdfRouteHandler } from './htmlToPdfRouteHandler';
 import * as express from 'express';
 import * as cors from 'cors';
 import { json } from 'body-parser';
 import { PORT } from './config';
+import { expressLogger } from './tools/logger';
 
 if (typeof process.env.SLIMERJSLAUNCHER === 'undefined') {
     throw new Error(
@@ -12,6 +12,8 @@ if (typeof process.env.SLIMERJSLAUNCHER === 'undefined') {
 }
 
 const app = express();
+
+app.use(expressLogger('pdf-maker'));
 
 app.use(json());
 app.use(cors());
